5da624df4a00da60a80577db71a787ef09b1a6c7
Diego Randolp <diego.quispe.ap@utec.edu.pe>
2025-03-15 23:52:16 -0500

    ✨ Feat: duplicates

--- p1.sql
+++ p1.sql
@@ -19,12 +19,38 @@ CREATE TABLE layoffs(
 );
 
 -- cp /home/.../layoffs.csv /tmp/
-
-
-\copy layoffs FROM '/home/diegorandolp/Code/NoUTEC/Data/strata_mod/layoffs.csv' DELIMITER ',' CSV HEADER
+-- In terminal:
+-- psql -U postgres -d data_analysis
+-- set search_path to cleaning;
+-- \copy layoffs FROM '/tmp/layoffs.csv' WITH (FORMAT csv, HEADER, DELIMITER ',', NULL 'NULL');
 
 SELECT *
 FROM layoffs
 LIMIT 5;
 
+-- 1. Duplicate
+-- 2. Standardize the data
+-- 3. Null or blank values
+-- 4. Remove columns
+
+CREATE TABLE layoffs_staging AS
+SELECT *
+FROM layoffs;
+
+
+-- 1. Duplicate: Partition by all columns
+
+CREATE TABLE layoffs_no_duplicates AS
+SELECT *,
+       ROW_NUMBER() OVER(
+           PARTITION BY company, location, industry, total_laid_off, percentage_laid_off,
+               stage, country, funds_raised_millions
+           ) AS row_num
+FROM layoffs_staging;
+
+DELETE FROM layoffs_no_duplicates
+WHERE row_num > 1;
+
+SELECT COUNT(*)
+FROM layoffs_no_duplicates;
 
