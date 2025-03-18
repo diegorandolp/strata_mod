a613fb410d8eb3f65bc55c482b7ca029df16019b
Diego Randolp <diego.quispe.ap@utec.edu.pe>
2025-03-18 17:06:43 -0500

    ✨ Feat: finish clean

--- p1.sql
+++ p1.sql
@@ -31,7 +31,7 @@ LIMIT 5;
 -- 1. Duplicate
 -- 2. Standardize the data
 -- 3. Null or blank values
--- 4. Remove columns
+-- 4. Remove columns/rows
 
 CREATE TABLE layoffs_staging AS
 SELECT *
@@ -54,3 +54,86 @@ WHERE row_num > 1;
 SELECT COUNT(*)
 FROM layoffs_no_duplicates;
 
+-- 2. Standardize data
+-- Check every column with DISTINCT
+-- 2.1 Delete blank spaces
+SELECT DISTINCT t1.company
+FROM layoffs_no_duplicates AS t1
+ORDER BY t1.company DESC;
+
+UPDATE layoffs_no_duplicates
+SET company = TRIM(company);
+
+-- 2.2 Standardize the name referring the same thing
+
+SELECT DISTINCT t1.industry
+FROM layoffs_no_duplicates AS t1
+ORDER BY t1.industry;
+
+-- Determine the names with the least frequency 'Crypto...'
+SELECT t1.industry, COUNT(*)
+FROM layoffs_no_duplicates AS t1
+WHERE t1.industry LIKE 'Crypto%'
+GROUP BY t1.industry;
+
+UPDATE layoffs_no_duplicates
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+--
+SELECT DISTINCT t1.country, TRIM(TRAILING '.' FROM t1.country)
+FROM layoffs_no_duplicates AS t1
+ORDER BY t1.country;
+
+UPDATE layoffs_no_duplicates
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE '%.';
+
+SELECT * from layoffs_no_duplicates LIMIT 5;
+
+-- 3. Null or blank values
+-- Try to complete the missing values with the information from the same company
+
+-- Check the representation of the missing values
+SELECT DISTINCT t1.industry
+FROM layoffs_no_duplicates as t1
+ORDER BY t1.industry;
+
+-- Check the companies with missing values
+SELECT *
+FROM layoffs_no_duplicates AS t1
+WHERE t1.industry IS NULL OR t1.industry = '';
+
+-- Standardize the missing values
+UPDATE layoffs_no_duplicates
+SET industry = NULL
+WHERE industry = '';
+
+-- Check the companies that have missing anc complete values of industry
+SELECT t1.industry, t2.industry
+FROM layoffs_no_duplicates AS t1
+JOIN layoffs_no_duplicates AS t2
+ON t1.company = t2.company
+WHERE t2.industry IS NULL
+AND t1.industry IS NOT NULL;
+
+-- Complete the missing values converting the previous query into an UPDATE
+UPDATE layoffs_no_duplicates AS t2
+SET industry = t1.industry
+FROM layoffs_no_duplicates AS t1
+WHERE t1.company = t2.company -- Join condition
+AND t2.industry IS NULL -- WHERE condition
+AND t1.industry IS NOT NULL;
+
+-- 4. Delete columns/rows
+DELETE
+FROM layoffs_no_duplicates
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+ALTER TABLE layoffs_no_duplicates
+DROP COLUMN row_num;
+
+SELECT *
+FROM layoffs_no_duplicates
+LIMIT 5;
